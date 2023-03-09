11d7ffc970692341454013c5621ea83fc55de4b2
임규연 <lky473736@icloud.com>
2023-03-09 20:42:04 +0900

    Add files via upload

---
+++ B0307-1.py
@@ -0,0 +1,13 @@
+#rad = int (input ("입력 = ")) # 입력받기 (input 겉에 껍질은 자료형에 따라 다름)
+# int = interger
+
+import math # 라이브러리 math 불러오기
+# C : #include <math.h> (math 헤더파일 불러오기)
+rad = 6
+area = 3.14 * rad * rad # 영역 넓이
+peri = 2 * 3.14 * rad # 영역 원주
+print ("area = ", area)
+print ("peri = ", peri)
+print ("Math Rst = ", math.sin(math.radians(30)))
+print(math.sqrt(10))
+
