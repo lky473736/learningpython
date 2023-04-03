22ca579fb749ea0f75f37a4e5281ac0aac38b151
임규연 <lky473736@icloud.com>
2023-04-03 11:28:28 +0900

    Add files via upload

---
+++ B0403-2.py
@@ -0,0 +1,10 @@
+import random
+
+sel = random.randint(0,1) # randint 함수 : 괄호 안 range 범위 안의 수 중 컴퓨터가 무작위의 수를 변수에 저장함
+
+if sel == 0 :
+    print ("앞면")
+    
+elif sel == 1 : 
+    print ("뒷면")
+    
