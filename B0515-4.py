7b90a82957136a65c67db17d9ba0b65524ae6b27
임규연 <lky473736@icloud.com>
2023-05-15 11:54:21 +0900

    Add files via upload

--- B0515-4.py
+++ B0515-4.py
@@ -1,11 +1,15 @@
 class tv : 
+    ok = 500 # class members : ok
+    
     def __init__ (self, chan = 10, vol = 10, state = "on") :
         self.chan = chan
         self.vol = vol
         self.state = state
+        self.ok = tv.ok * 5 # tv.ok = 500
+        # class members는 class이름.variable이름 으로 구성된다.
         
     def show (self) :
-        print (self.chan, self.vol, self.state)
+        print (self.chan, self.vol, self.state, self.ok)
         
 tv1 = tv()
 tv1.show()
@@ -24,4 +28,4 @@ tv1.show()
 
 # class members : 모든 객체를 통틀어서 하나만 생성되고 모든 객체가 이것을 공유하는 변수
 # 지역변수 (instance variable) vs class에서 전역변수 (class members)
-# class members는 항상 class에서 선언하게 된다
+# class members는 항상 class에서 선언하게 된다 (위의 ok 참고)
